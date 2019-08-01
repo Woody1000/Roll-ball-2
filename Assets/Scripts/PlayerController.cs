d8ec17ae293252d0593c518be9ef9e0063493ee4
Woody1000 <yazhivunazemle@gmail.com>
2019-08-01 10:21:23 +0500

    бесконечное создание платформ

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,31 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
+    public bool grounded;
+    public LayerMask whatIsGround;
+
+    private Rigidbody2D _myRigidbody2D;
+    private Collider2D _myCollider;
          
     void Start()
     {
-        
+        _myRigidbody2D = GetComponent<Rigidbody2D>();
+        _myCollider = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        grounded = Physics2D.IsTouchingLayers(_myCollider, whatIsGround);
+
+        _myRigidbody2D.velocity = new Vector2(moveSpeed, _myRigidbody2D.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if (grounded)
+            {
+                _myRigidbody2D.velocity = new Vector2(_myRigidbody2D.velocity.x, jumpForce);
+            }
+        }
     }
 }
