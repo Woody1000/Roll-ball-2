218b7687b098758a1598746b6d812b41b6160303
Woody1000 <yazhivunazemle@gmail.com>
2019-08-04 11:50:13 +0500

    Уничтожение платформ за игроком. Начал работать над кодом по созданию препядствий

---
+++ Assets/Scripts/ObstacleCreation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleCreation : MonoBehaviour
+{
+    public float timer;
+    Random rand = new Random();
+    public float timeRange;
+    public GameObject generationPoint_1;
+    public GameObject generationPoint_2;
+    public GameObject prefabObstacle;
+
+    private GameObject _obstacles;
+    
+    void Start()
+    {
+        SpawnObstacles();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = timer + Time.deltaTime;
+    }
+
+
+    public void SpawnObstacles()
+    {
+        _obstacles = Instantiate(prefabObstacle, transform);
+        Vector3 positionObs = new Vector3(Random.Range(generationPoint_1.transform.localPosition.x, generationPoint_2.transform.localPosition.x), generationPoint_1.transform.localPosition.y, 0);
+        _obstacles.transform.localPosition = positionObs;
+
+    }
+
+    public void ResetTimerAndSpawn()
+    {
+        if (timer > timeRange)
+        {
+            SpawnObstacles();
+            timer = 0;
+        }
+    }
+
+}
--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -7,6 +7,12 @@ public class PlatformGenerator : MonoBehaviour
     public GameObject thePlatform;
     public Transform generationPoint;
     public float distanceBetween;
+    public float timer;
+    public float timeRange;
+    public int numberWhenCreate;
+    public GameObject prefabObstacle;
+
+    Random rand = new Random();
 
     private float _platformWidth;
     void Start()
@@ -17,11 +23,38 @@ public class PlatformGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer = timer + Time.deltaTime;
+
         if(transform.position.x < generationPoint.position.x)
         {
             transform.position = new Vector3(transform.position.x + _platformWidth + distanceBetween, transform.position.y, transform.position.z);
 
             Instantiate(thePlatform, transform.position, transform.rotation);
+        }
+
+        CreateObstacle();
+
+        
+    }
+
+    public void CreateObstacle()
+    {
+        if(timer > timeRange)
+        {
+            
+            timer = 0;
+            if (timer == 0)
+            {
+                numberWhenCreate = Random.Range(1, 4);
+            }
+
+            
+        }
+
+        if (numberWhenCreate == 2)
+        {
+            transform.position = new Vector3(thePlatform.transform.position.x, thePlatform.transform.position.y + 1, thePlatform.transform.position.z);
+            Instantiate(prefabObstacle, transform.position, transform.rotation);
         }
     }
 }
